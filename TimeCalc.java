6df5cb769755b40fce1c38d56deda7d4db1dabee
NivDorfman <nivdorf@gmail.com>
2024-11-14 09:24:55 +0200

    Update TimeCalc.java

--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,13 @@
 public class TimeCalc {
     public static void main(String[] args) {
-        // Replace this comment with your code
-    }
+           int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
+           int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
+           int minuteAdd = Integer.parseInt(args[1]);
+        
+           minutes += hours*60 +minuteAdd;
+           hours = minutes/60;
+           int newHour = hours % 24;
+           int newMinutes = minutes - hours * 60;
+
+           System.out.println(newHour + ":" + newMinutes);    }
 }
